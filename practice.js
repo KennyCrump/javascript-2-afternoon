52429f71c947d50b3e20ae669103688ef01f6c18
KennyCrump <kennycrump9@hotmail.com>
2018-08-14 15:00:17 -0600

    finished problems

--- practice.js
+++ practice.js
@@ -14,8 +14,11 @@
   Then alert your name using dot notation.
 */
 
-//Code here
-
+var me = {
+  name : "Kenny",
+  age : 28
+}
+//alert(me.name);
 
 
 ////////// PROBLEM 2 //////////
@@ -25,7 +28,14 @@
   Have the values to those keys be your favorite thing in that category.
 */
 
-//Code here
+var favoriteThings = {
+  band : "Foo Fighters",
+  food: "Sushi",
+  person: "Cassie",
+  book: "Count of Monte Cristo",
+  movie: "About Time",
+  holiday: "Christmas"
+}
 
 
 
@@ -33,7 +43,8 @@
   After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 */
 
-//Code here
+favoriteThings.car = "Audi R8";
+favoriteThings.brand = "Nike";
 
 
 
@@ -41,7 +52,9 @@
   Now change the value of the food key in your favoriteThings object to be 'Chicken Nuggets' and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
 
-//Code here
+favoriteThings.food = "Chicken Nuggets";
+favoriteThings.book = "Harry Potter";
+
 
 
 
@@ -55,7 +68,10 @@
   Using dot notation, add another key (or property) to your backPack object that is named color, with the value being the color of your backpack. 
 */
 
-//Code here
+var backPack = {}
+item = "firstPocket";
+backPack[item] = "chapstick";
+backPack.color = "blue";
 
 
 
@@ -63,7 +79,7 @@
   After you do the above, alert your entire backPack object.
 */
 
-//Code here
+// alert(backPack)
 
 
 
@@ -73,7 +89,7 @@ Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console. 
 */
 
-//Code here
+console.log(backPack);
 
 
 
@@ -96,7 +112,9 @@ var user2 = {
   Make that change without modifying the original object code above.
 */
 
-//Code Here
+user2.name = "Tyler S. McGinnis";
+user2.email = "tyler.mcginnis@devmounta.in";
+
 
 
 
@@ -113,51 +131,65 @@ var user2 = {
 
 
 
-////////// PROBLEM 5 //////////
+///////// PROBLEM 5 //////////
 
 /*
-  Create an empty object called methodCollection.
+ Create an empty object called methodCollection.
 */
 
-//Code Here
+var methodCollection = {
 
-
-
-/*
-  Now add two methods (functions that are properties on objects) to your methodCollection object. 
-  One called 'alertHello' which alerts 'hello' and another method called 'logHello' which logs 'hello' to the console. 
-*/
-
-//Code Here
+}
 
 
 
 /*
-  Now call your alertHello and logHello methods.
+ Now add two methods (functions that are properties on objects) to your methodCollection object.
+ One called 'alertHello' which alerts 'hello' and another method called 'logHello' which logs 'hello' to the console.
 */
 
-//Code Here
+methodCollection.alertHello = function(){alert("hello")}
+methodCollection.logHello = function(){console.log("hello")}
+
+
+
+/*
+ Now call your alertHello and logHello methods.
+*/
+
+methodCollection.alertHello()
+methodCollection.logHello()
 
 
 
 ////////// PROBLEM 6 //////////
 
-/* 
-  Create a function called makePerson which takes in name, birthday, ssn as its parameters. 
-  Return a new object with all of the information that you passed in.
+/*
+ Create a function called makePerson which takes in name, birthday, ssn as its parameters.
+ Return a new object with all of the information that you passed in.
 */
 
-//Code Here
+function makePerson(name, birthday, ssn) {
+ return {
+   "name": name,
+   "birthday": birthday,
+   "ssn": ssn
+ }
+}
 
 
 
 ////////// PROBLEM 7 //////////
 
 /*
-  Create a function called makeCard which takes in cardNumber, expirationDate, and securityCode to make a Credit Card object.
-  Return that object so that whenever you invoke makeCard, you get a brand new credit card.
+ Create a function called makeCard which takes in cardNumber, expirationDate, and securityCode to make a Credit Card object.
+ Return that object so that whenever you invoke makeCard, you get a brand new credit card.
 */
 
-//Code Here
-
-
+function makeCard(cardNumber, expirationDate, securityCode){
+ return {
+   "Card Number": cardNumber,
+   "Expiration Date": expirationDate,
+   "Security Code": securityCode
+ }
+}
